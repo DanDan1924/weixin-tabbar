3988e1a21ce2e63bc539473fa8fd82a1036c7a5e
吴晶晶 <wujingjing@owhat.cn>
2019-09-24 15:14:24 +0800

    提交

--- pages/components/tabBar/tabBar.js
+++ pages/components/tabBar/tabBar.js
@@ -1,66 +1,29 @@
-// pages/components/tabBar/tabBar.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
+var types
+// const utils = require('../../utils/util.js')
+var app = getApp()
+Component({
+  options: {
+    multipleSlots: true
+  },
+  properties: {
+    rootPath: String
+  },
   data: {
-
+    url: '',
+    showMore: false,
+    imgUrl: '/images/'
   },
+  methods: {
+    switchMian: function(e) {
+      let pathStr = e.target.dataset.path
+      
+      if (pathStr != this.data.rootPath) {
+        this.triggerEvent('rootPath', { rootPath: pathStr, title: e.target.dataset.title })
+      }
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      wx.setNavigationBarTitle({
+        title: e.target.dataset.title
+      })
+    },
   }
 })
